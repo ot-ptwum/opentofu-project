a2d3c34a3577402fc3275e5f500386d82ec34b83
ot-ptwum <ptwum@orangetech.io>
2026-02-27 10:57:06 +0000

    update: modularised the project

---
+++ environments/test/outputs.tf
@@ -0,0 +1,15 @@
+output "service_url" {
+  value = module.app_runner.service_url
+}
+
+output "service_arn" {
+  value = module.app_runner.service_arn
+}
+
+output "service_status" {
+  value = module.app_runner.service_status
+}
+
+output "custom_domain_validation_records" {
+  value = module.app_runner.custom_domain_validation_records
+}
